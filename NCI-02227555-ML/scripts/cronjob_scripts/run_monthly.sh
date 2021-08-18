c2beba416710c0a65ca6260e11df72c1b32d2aca
kopardev <vishal.koparde@nih.gov>
2021-08-18 15:02:33 -0400

    monthly push on 081821

---
+++ NCI-02227555-ML/scripts/cronjob_scripts/run_monthly.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+# get date
+dt=$(date +"%D"|sed "s@/@@g")
+
+# Backup crontab
+crontab -l > /Users/kopardevn/scripts/cronjob_scripts/crontab.bak
+# you can load from this backup with
+# crontab /Users/kopardevn/scripts/cronjob_scripts/crontab.bak
+
+
+# Push changes to github
+cd /Users/kopardevn/scripts/
+git add .
+git commit -m "monthly push on $dt"
+git push
