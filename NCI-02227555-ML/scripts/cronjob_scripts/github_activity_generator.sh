85613981d91fe4338835365331a16084224baf16
kopardev <vishal.koparde@nih.gov>
2021-08-19 14:28:41 -0400

    addin github_activity_generator.sh

---
+++ NCI-02227555-ML/scripts/cronjob_scripts/github_activity_generator.sh
@@ -0,0 +1,11 @@
+#!/bin
+# based off of https://github.com/kopardev/activitygenerator
+# cd to clone location
+cd ~/Desktop/GitRepos/activitygenerator
+# run script to generate random string in file "random_uuid.txt"
+bash create_random_uuid.sh
+# commit the new file to github
+x=$(cat random_uuid.txt )
+git add .
+git commit -m "$x"
+git push
