7070a72da59be63ecbf927f6ff4062265ddbc886
Vishal Koparde <vishal.koparde@nih.gov>
2021-08-23 16:47:01 -0400

    adding terminal notifier

--- NCI-02227555-ML/scripts/cronjob_scripts/runthisscript_and_logit.sh
+++ NCI-02227555-ML/scripts/cronjob_scripts/runthisscript_and_logit.sh
@@ -1,11 +1,13 @@
-#!/bin/bash
+#!/usr/bin/env bash
 # wrapper script to run script with proper logging
 # arguments:
 # arg1: path of script to run and its arguments
 
+notifier=/usr/local/bin/terminal-notifier
+
 # setting some basics for logging
 # scriptname=$(basename `echo $0`)
-scriptname=$(basename $2)
+scriptname=$(basename $1)
 dt=$(date +"%D_%T"|sed "s@/@@g"|sed "s@:@@g")
 scriptrunlog_bn="${scriptname}.${dt}.log"
 # define CRONJOB_LOGDIR in .zshrc or .bashrc
@@ -17,3 +19,7 @@ echo $scriptrunlog
 
 # run the script
 $@ > $scriptrunlog 2>&1
+
+now=$(date)
+$notifier -title CronJob -subtitle "$now" -message "Job Ran: $scriptname" 
+echo "Cron job $scriptname completed at $now"
