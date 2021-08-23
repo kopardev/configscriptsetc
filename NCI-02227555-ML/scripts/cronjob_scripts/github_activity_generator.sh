65d27ff768aa445c59b5cfdeedc69bb7897c3713
Vishal Koparde <vishal.koparde@nih.gov>
2021-08-23 16:46:36 -0400

    differnce in readlink for unix vs mac

--- NCI-02227555-ML/scripts/cronjob_scripts/github_activity_generator.sh
+++ NCI-02227555-ML/scripts/cronjob_scripts/github_activity_generator.sh
@@ -1,8 +1,14 @@
-#!/bin/bash
+#!/usr/bin/env bash
 
 # based off of https://github.com/kopardev/activitygenerator
+if [ "$(uname)" == "Darwin" ];then
+rl=$(which greadlink)
+elif [ "$(uname)" == "Linux" ];then
+rl=$(which readlink)
+fi
+
 # cd to clone location
-cd ~/Desktop/GitRepos/activitygenerator
+cd $(dirname $($rl -f $(basename $0)))
 # run script to generate random string in file "random_uuid.txt"
 bash create_random_uuid.sh
 # commit the new file to github
