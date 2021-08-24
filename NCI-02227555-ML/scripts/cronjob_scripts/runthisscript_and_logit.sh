f6151020da36a0d4b49067115920883762ad55e1
Vishal Koparde <vishal.koparde@nih.gov>
2021-08-23 20:22:26 -0400

    CRONJOB_LOGDIR env variable is not getting parsed in the bash shell for cronjobs ... hence setting it in the wrapper script

--- NCI-02227555-ML/scripts/cronjob_scripts/runthisscript_and_logit.sh
+++ NCI-02227555-ML/scripts/cronjob_scripts/runthisscript_and_logit.sh
@@ -4,6 +4,8 @@
 # arg1: path of script to run and its arguments
 
 notifier=/usr/local/bin/terminal-notifier
+# if notifier is not installed then
+# brew install terminal-notifier
 
 # setting some basics for logging
 # scriptname=$(basename `echo $0`)
@@ -11,8 +13,9 @@ scriptname=$(basename $1)
 dt=$(date +"%D_%T"|sed "s@/@@g"|sed "s@:@@g")
 scriptrunlog_bn="${scriptname}.${dt}.log"
 # define CRONJOB_LOGDIR in .zshrc or .bashrc
-logdir=$CRONJOB_LOGDIR
-if [ "$logdir" == "" ];then logdir=$HOME;fi
+# logdir=$CRONJOB_LOGDIR
+# if [ "$logdir" == "" ];then logdir=$HOME;fi
+logdir="/Users/kopardevn/scripts/cronjob_scripts/logs"
 scriptrunlog="${logdir}/${scriptrunlog_bn}"
 
 echo $scriptrunlog
