73001c1cb6f28bbbc1a38d326b45d38c66eedd47
Vishal Koparde <vishal.koparde@nih.gov>
2021-08-23 20:31:07 -0400

    clone dir is now hardcoded

--- NCI-02227555-ML/scripts/cronjob_scripts/github_activity_generator.sh
+++ NCI-02227555-ML/scripts/cronjob_scripts/github_activity_generator.sh
@@ -9,11 +9,12 @@ fi
 
 
 # cd to clone location
-cd $(/usr/bin/dirname $($rl -f $(/usr/bin/basename $0)))
+clone_dir="/Users/kopardevn/Documents/GitRepos/activitygenerator"
+cd $clone_dir
 # run script to generate random string in file "random_uuid.txt"
-/usr/bin/bash create_random_uuid.sh
+/bin/bash create_random_uuid.sh
 # commit the new file to github
-x=$(/usr/bin/cat random_uuid.txt )
+x=$(/bin/cat random_uuid.txt )
 /usr/bin/git add .
 /usr/bin/git commit -m "$x"
 /usr/bin/git push
