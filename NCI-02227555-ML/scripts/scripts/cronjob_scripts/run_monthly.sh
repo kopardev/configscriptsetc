b652150bbc06de5e57470cbd2544ee99600b0631
kopardev <vishal.koparde@nih.gov>
2024-10-05 17:40:28 -0400

    push on 100524

--- NCI-02227555-ML/scripts/scripts/cronjob_scripts/run_monthly.sh
+++ NCI-02227555-ML/scripts/scripts/cronjob_scripts/run_monthly.sh
@@ -8,6 +8,7 @@
 # initially this script was run 1st of every month at midnight
 # now, it is being run every Tuesday at 10:20am
 
+debug=1
 BASEFOLDER="/Users/kopardevn/Documents/GitRepos/configscriptsetc"
 MACHINENAME="NCI-02279157-ML"
 
@@ -17,15 +18,24 @@ set -exo pipefail
 dt=$(date +"%D"|sed "s@/@@g")
 
 # Backup crontab
+if [[ "$debug" == "1" ]];then 
+echo "Backing up crontab"
+fi
 crontab -l > ${BASEFOLDER}/${MACHINENAME}/scripts/cronjob_scripts/crontab.bak/crontab.bak.$dt
 # you can load from this backup with
 # crontab /Users/kopardevn/scripts/cronjob_scripts/crontab.bak
 
 # Backup conda envs
+if [[ "$debug" == "1" ]];then 
+echo "Backing up conda envs"
+fi
 cd $BASEFOLDER/$MACHINENAME
 ./backup_conda_envs.sh
 
 # copy configurations
+if [[ "$debug" == "1" ]];then 
+echo "Copying configurations"
+fi
 cp ~/.exrc .
 cp ~/.vmrc .
 cp ~/.gitconfig .
@@ -34,6 +44,9 @@ cp ~/.zshrc_kopardevn .
 
 
 # Push changes to github
+if [[ "$debug" == "1" ]];then 
+echo "Pushing changes to github"
+fi
 cd $BASEFOLDER
 git add .
 git commit -m "push on $dt"
